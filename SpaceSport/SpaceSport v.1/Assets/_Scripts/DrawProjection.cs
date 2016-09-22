8d242a296deeee2ff357356c4ef7f5fa8c6c61e3
Max <jmwatki2@illinois.edu>
2016-09-22 10:57:31 -0500

    Import project folder

---
+++ SpaceSport/SpaceSport v.1/Assets/_Scripts/DrawProjection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawProjection : MonoBehaviour {
+
+	public Object prefab;
+	private Object refr;
+	// Use this for initialization
+	void Start () {
+		refr = Instantiate (prefab, TargetHelper.getPositionIn (GetComponent<Rigidbody> (), 10), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy (refr);
+		refr = Instantiate (prefab, TargetHelper.getPositionIn (GetComponent<Rigidbody> (), 10), Quaternion.identity);
+	}
+}
---
+++ SpaceSport/SpaceSport v.1/Assets/_Scripts/MovementScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+	public int maxAcc;
+	public int maxSpeed;
+	// Use this for initialization
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.transform.position = new Vector3(0f,0f,0f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//var test = rb.GetRelativePointVelocity(Vector3(0f,0f,0f));
+		float accelerate = Input.GetAxis ("Vertical");
+		float strafe = Input.GetAxis ("Horizontal");
+		float rise = Input.mouseScrollDelta.y;
+
+		if (accelerate < 0)
+			accelerate *= .5f;
+		float rotationX = Input.GetAxis ("Mouse X");
+		float rotationY = Input.GetAxis ("Mouse Y");
+
+		rb.transform.Rotate(new Vector3 (-2 * rotationY, 2 * rotationX, 0));
+	
+		rb.AddRelativeForce(new Vector3(strafe, rise, accelerate )*maxAcc);
+	}
+}
